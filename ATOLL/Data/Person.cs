9056cab2f32fac551f662299b96d24ad73e1e7b5
DME <DME@DESKTOP-VOHEI6G>
2022-04-07 09:19:12 +0200

    Ajoutez des fichiers projet.

---
+++ ATOLL/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATOLL.Data
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public double Height { get; set; }
+    }
+}
---
+++ ATOLL/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using ATOLL.Data;
+using Syncfusion.Blazor;
+
+
+namespace ATOLL;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjA1MjI2QDMyMzAyZTMxMmUzMFZnc09NYmYydFl2cStVT09lOFIzMHlIbzY2RHc0cS9Jdk1LT294eFJjUFk9");	
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.RegisterBlazorMauiWebView()
+			.UseMauiApp<App>()
+			
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+		builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+		builder.Services.AddBlazorWebView();
+		builder.Services.AddSingleton<WeatherForecastService>();
+		
+		return builder.Build();
+	}
+}
